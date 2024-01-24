9e724d5a881b4dce4e085060b2b6d41cbe724399
cozumburda <acozumelektronik@gmail.com>
2024-01-24 15:23:04 +0300

    ders 54

--- Acozum_Dpr_Estate_Api/Hubs/SignalRHub.cs
+++ Acozum_Dpr_Estate_Api/Hubs/SignalRHub.cs
@@ -1,8 +1,22 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Net.Http;
 
 namespace Acozum_Dpr_Estate_Api.Hubs
-{
+{    
     public class SignalRHub : Hub
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public SignalRHub(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task SendCategoryCount()
+        {
+            var client1 = _httpClientFactory.CreateClient();
+            var responseMessage1 = await client1.GetAsync("https://localhost:44371/api/Statistics/CategoryCount");
+            var value1 = await responseMessage1.Content.ReadAsStringAsync();
+            await Clients.All.SendAsync("ReceiveCategoryCount",value1);
+        }
     }
 }
---
+++ Acozum_Dpr_Estate_UI/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Acozum_Dpr_Estate_UI.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
